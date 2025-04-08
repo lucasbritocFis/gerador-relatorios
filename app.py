580c8acc11580f29fa4410c851ca1c68bf152a6a
lucasbritocFis <lucasdbcavalcanti@gmail.com>
2025-04-08 07:46:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,33 @@ from datetime import datetime
 import pdfplumber
 from reportlab.lib import colors
 
+st.set_page_config(page_title="Portal Interno", layout="centered")
+st.title("🖥️ Acesso Rápido - Portal Interno")
+
+# Citrix
+st.markdown("### 💻 Abrir Citrix StoreWeb")
+citrix_url = "http://bronbsv004app.adhosp.com.br/Citrix/StoreWeb/"
+st.markdown(f"[Abrir Citrix]({citrix_url})", unsafe_allow_html=True)
+
+# Tasy
+st.markdown("### 💻 Abrir Tasy")
+tasy_url = "https://tasyprd.adhosp.com.br/#/login"
+st.markdown(f"[Abrir Tasy]({tasy_url})", unsafe_allow_html=True)
+
+st.markdown("### 📊 Abrir Planilha Excel (rede interna)")
+
+# Caminho da planilha (use \\ para Windows ou / para Linux se necessário)
+planilha_path = r"file://10.50.90.18/Radioterapia/PORTAL-RADIOTERAPIA.xlsb"
+
+# Link clicável para abrir a planilha
+st.markdown(f"""
+    <a href="{planilha_path}" target="_blank" style='text-decoration:none;'>
+        <button style='padding:10px 20px; font-size:16px; background-color:#4CAF50; color:white; border:none; border-radius:8px; cursor:pointer;'>
+            Abrir Planilha Excel
+        </button>
+    </a>
+""", unsafe_allow_html=True)
+
 # Função para extrair imagens e texto do PDF de Relatório de Tratamento
 def extrair_imagens_e_texto(pdf_file):
     doc = fitz.open(stream=pdf_file.read(), filetype="pdf")
