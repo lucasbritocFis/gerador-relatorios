397f660ef43ca352ae818ccd477e8c267d433c65
lucasbritocFis <lucasdbcavalcanti@gmail.com>
2025-04-08 07:48:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,23 +9,6 @@ from datetime import datetime
 import pdfplumber
 from reportlab.lib import colors
 
-st.set_page_config(page_title="Portal Interno", layout="centered")
-st.title("🖥️ Acesso Rápido - Portal Interno")
-
-# Citrix
-st.markdown("### 💻 Abrir Citrix StoreWeb")
-citrix_url = "http://bronbsv004app.adhosp.com.br/Citrix/StoreWeb/"
-st.markdown(f"[Abrir Citrix]({citrix_url})", unsafe_allow_html=True)
-
-# Tasy
-st.markdown("### 💻 Abrir Tasy")
-tasy_url = "https://tasyprd.adhosp.com.br/#/login"
-st.markdown(f"[Abrir Tasy]({tasy_url})", unsafe_allow_html=True)
-
-st.markdown("### 📊 Abrir Planilha Excel (rede interna)")
-
-# Caminho da planilha (use \\ para Windows ou / para Linux se necessário)
-planilha_path = r"file://10.50.90.18/Radioterapia/PORTAL-RADIOTERAPIA.xlsb"
 
 
 
